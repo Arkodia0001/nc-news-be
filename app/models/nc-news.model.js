8e78455f9cfaeb13163c820290ee5913aa003b99
Arkodia0001 <149802973+Arkodia0001@users.noreply.github.com>
2024-02-22 14:29:34 +0000

    completed task 9-delete-comment-by-id

--- app/models/nc-news.model.js
+++ app/models/nc-news.model.js
@@ -92,3 +92,11 @@ exports.updateArticleByID = (update, article_id) => {
     return rows[0];
   });
 };
+
+exports.deleteCommentByID = (comment_id) => {
+    return db.query('DELETE FROM comments WHERE comment_id = $1', [comment_id]).then(({rowCount}) => {
+        if(rowCount === 0){
+            return Promise.reject({status: 404, msg: 'Comment does not exist!'})
+        }
+    })
+}
